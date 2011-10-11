f3b5985d0adfd8c533b58a8931f7559a06b7fc12
Ryan Connelly <ryanconnelly@Ryans-MacBook-Pro.local>
2011-10-10 23:07:28 -0400

    Renamed files to match apple category naming convention.

---
+++ StackMob/StackMobAdditions.h
@@ -0,0 +1,10 @@
+//
+//  StackMobAdditions.h
+//  StackMobiOS
+//
+//  Created by Ryan Connelly on 10/9/11.
+//  Copyright (c) 2011 StackMob, Inc. All rights reserved.
+//
+
+#import "NSDictionary+StackMob.h"
+#import "NSArray+StackMob.h"
