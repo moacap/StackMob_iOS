700e4eee510a403625fe8ef1bcf3ac78f188409e
Ryan Connelly <ryan@nomadapps.net>
2011-12-14 17:55:13 -0500

    updated demo application

---
+++ Demo/ReskitDemoApp/ReskitDemoAppTests/ReskitDemoAppTests.m
@@ -0,0 +1,32 @@
+//
+//  ReskitDemoAppTests.m
+//  ReskitDemoAppTests
+//
+//  Created by Erica Connelly on 11/3/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "ReskitDemoAppTests.h"
+
+@implementation ReskitDemoAppTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in ReskitDemoAppTests");
+}
+
+@end
